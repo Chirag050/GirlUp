0d65385fd1392a5f8c7191ca9241cfdf229b9462
Chirag050 <148074355+Chirag050@users.noreply.github.com>
2025-08-23 23:12:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
       const form = e.target;
       const data = new FormData(form);
-      const action = "https://script.google.com/macros/s/AKfycbyCqVFm0hexP7l8xJqDpdjPIDsl3FPvCweoiuN4u27kSlHFny-gqIs4jbAJDcv1jkoLOw/exec";
+      const action = "https://script.google.com/macros/s/AKfycbxXcX9OG5sjh2C6si0b_VaJUlQoc3IVMkOKxzhDI9K5kErRUO_GQvllRdIuylRkjZX50Q/exec";
 
       fetch(action, { method: "POST", body: data })
         .then(() => {
@@ -35,3 +35,4 @@
         }
       });
     });
+
